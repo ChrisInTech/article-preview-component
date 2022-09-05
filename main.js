373bfe2bb2e44bf99c2a9749e8f4476a9159ec25
Christoph <christoph.snger@yahoo.com>
2022-09-05 16:43:45 +0200

    ..

--- main.js
+++ main.js
@@ -1,2 +1,7 @@
-const shareBtn = document.getElementsByClassName('share_btn')
+const shareBtn = document.getElementsByClassName('share_btn')[0]
+const iconBox = document.getElementsByClassName('icon_box')[0]
 
+
+shareBtn.addEventListener('click', ()=>{
+    if()
+})
