b559dd71264fa83d49e707c558fefae5c3de0b55
Christoph <christoph.snger@yahoo.com>
2022-09-10 11:24:31 +0200

    ..

--- main.js
+++ main.js
@@ -2,6 +2,4 @@ const shareBtn = document.getElementsByClassName('share_btn')[0]
 const iconBox = document.getElementsByClassName('icon_box')[0]
 
 
-shareBtn.addEventListener('click', ()=>{
-    if()
-})
+
