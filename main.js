3094f2f06c577dc74bf9dea96cf0f3d176dea2b4
Christoph <christoph.snger@yahoo.com>
2022-09-10 12:11:14 +0200

    fertig

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
-const shareBtn = document.getElementsByClassName('share_btn')[0]
-const iconBox = document.getElementsByClassName('icon_box')[0]
+const openBox = document.getElementsByClassName('share_btn')[0]
+const open = document.getElementById('open')
+const closeBox = document.getElementById('close')
+const triangle = document.getElementById('triangle')
+
+const openIconBox = () =>{
+    open.style.display = 'block'
+
+}
+
+const closeIconBox = () =>{
+    open.style.display = 'none'
+}
+
+openBox.addEventListener('click', openIconBox)
+closeBox.addEventListener('click', closeIconBox)
+
 
 
 
